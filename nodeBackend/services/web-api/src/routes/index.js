917a2e978a9eeb1e88bae084784cbae260124a87
mrranger939 <mdshujathnawaz@gmail.com>
2026-01-31 23:17:53 +0530

    feat: Implement authentication routes and controllers for signup and login

--- nodeBackend/services/web-api/src/routes/index.js
+++ nodeBackend/services/web-api/src/routes/index.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
+import { tokenRequired } from "../middleware/auth.js";
+import authRoutes from "./auth.js";
+
 const router = Router();
+
+router.use("/", authRoutes);
 
 router.get("/", (req, res) => {
   res.send("Server is running");
+});
+
+router.get("/protected", tokenRequired, (req, res) => {
+  res.json({ message: "You are authorized", user: req.user });
 });
 
 export default router;
