a1445dffd8ab562ae4751ff9cc680683294a17bb
mrranger939 <mdshujathnawaz@gmail.com>
2026-02-01 18:46:16 +0530

    feat: Add video controller and routes for video data retrieval and streaming

--- nodeBackend/services/web-api/src/routes/index.js
+++ nodeBackend/services/web-api/src/routes/index.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { tokenRequired } from "../middleware/auth.js";
 import authRoutes from "./auth.js";
 import uploadRoutes from "./upload.js";
+import videoRoutes from "./videos.js";
 
 const router = Router();
 
 router.use("/", authRoutes);
-router.use("/", uploadRoutes);
+router.use("/", uploadRoutes);  
+router.use("/", videoRoutes);
 
 router.get("/", (req, res) => {
   res.send("Server is running");
